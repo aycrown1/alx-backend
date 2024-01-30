1523df9c8dfc2bc6b2ee89c89be5e450fa997f44
aycrown1 <ayoadeadedamola198@gmail.com>
2024-01-30 00:25:34 +0000

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,7 +10,7 @@ def index_range(page, page_size):
     """
     if page <= 0 or page_size <= 0:
         return None
-    
+
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
 
