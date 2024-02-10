c8510d88a1dff725286f23a70724b5b4917ece0b
aycrown1 <ayoadeadedamola198@gmail.com>
2024-02-10 03:28:00 +0000

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+
+@app.route('/')
+def index():
+    """
+    Flask index pag
+    """
+    return render_template('index.html', title='Welcome to Holberton', header='Hello world')
+
+if __name__ == '__main__':
+    app.run(debug=True)
